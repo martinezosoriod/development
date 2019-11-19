56edad22422bd5c2a1bb8faeef5396c330ff6d49
n3mod <n3mod@martinezosoriod@gmail.com>
2019-11-19 01:52:51 -0500

    ajuste index

--- index.php
+++ index.php
@@ -1,8 +1,32 @@
+<!doctype html>
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Invreate Space</title>
+    <meta name="description" content="">
+    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+    <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
-    <?php echo 'System'; ?>
+    <header>
+      <a href="#">
+        <img src="img/login.png" alt="Invreate Space">
+      </a>
+    </header>
+    <div id="container">
+      <div class="bottom">
+        <a href="system"><?php echo 'System'; ?></a>
+        <p></p>
+      </div>
+      <div class="bottom">
+        <a href="#"><?php echo ' '; ?></a>
+        <p></p>
+      </div>
+      <div class="bottom">
+        <a href="#"><?php echo ' '; ?></a>
+        <p></p>
+      </div>
+    </div>
   </body>
 </html>
