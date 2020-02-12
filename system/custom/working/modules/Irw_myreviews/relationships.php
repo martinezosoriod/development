74ac5631fc054999373a83b35fc379d08881c608
martinezosoriod <martinezosoriod@gmail.com>
2020-02-12 16:37:04 -0500

    relacion

---
+++ system/custom/working/modules/Irw_myreviews/relationships.php
@@ -0,0 +1,292 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+$relationships = array (
+  'irw_myreviews_modified_user' => 
+  array (
+    'id' => '7e8e693d-4d5a-4e2f-182f-5e446543f992',
+    'relationship_name' => 'irw_myreviews_modified_user',
+    'lhs_module' => 'Users',
+    'lhs_table' => 'users',
+    'lhs_key' => 'id',
+    'rhs_module' => 'Irw_myreviews',
+    'rhs_table' => 'irw_myreviews',
+    'rhs_key' => 'modified_user_id',
+    'join_table' => NULL,
+    'join_key_lhs' => NULL,
+    'join_key_rhs' => NULL,
+    'relationship_type' => 'one-to-many',
+    'relationship_role_column' => NULL,
+    'relationship_role_column_value' => NULL,
+    'reverse' => '0',
+    'deleted' => '0',
+    'readonly' => true,
+    'rhs_subpanel' => NULL,
+    'lhs_subpanel' => NULL,
+    'relationship_only' => false,
+    'for_activities' => false,
+    'is_custom' => false,
+    'from_studio' => true,
+  ),
+  'irw_myreviews_activities_notes' => 
+  array (
+    'id' => '830e62aa-3e25-8877-33cb-5e44654ca7ee',
+    'relationship_name' => 'irw_myreviews_activities_notes',
+    'lhs_module' => 'Irw_myreviews',
+    'lhs_table' => 'irw_myreviews',
+    'lhs_key' => 'id',
+    'rhs_module' => 'Notes',
+    'rhs_table' => 'notes',
+    'rhs_key' => 'parent_id',
+    'join_table' => NULL,
+    'join_key_lhs' => NULL,
+    'join_key_rhs' => NULL,
+    'relationship_type' => 'one-to-many',
+    'relationship_role_column' => 'parent_type',
+    'relationship_role_column_value' => 'Irw_myreviews',
+    'reverse' => '0',
+    'deleted' => '0',
+    'readonly' => true,
+    'rhs_subpanel' => NULL,
+    'lhs_subpanel' => NULL,
+    'is_custom' => true,
+    'relationship_only' => false,
+    'for_activities' => false,
+    'from_studio' => true,
+  ),
+  'irw_myreviews_created_by' => 
+  array (
+    'id' => '906f9d63-fa55-d009-a85c-5e44653689f9',
+    'relationship_name' => 'irw_myreviews_created_by',
+    'lhs_module' => 'Users',
+    'lhs_table' => 'users',
+    'lhs_key' => 'id',
+    'rhs_module' => 'Irw_myreviews',
+    'rhs_table' => 'irw_myreviews',
+    'rhs_key' => 'created_by',
+    'join_table' => NULL,
+    'join_key_lhs' => NULL,
+    'join_key_rhs' => NULL,
+    'relationship_type' => 'one-to-many',
+    'relationship_role_column' => NULL,
+    'relationship_role_column_value' => NULL,
+    'reverse' => '0',
+    'deleted' => '0',
+    'readonly' => true,
+    'rhs_subpanel' => NULL,
+    'lhs_subpanel' => NULL,
+    'relationship_only' => false,
+    'for_activities' => false,
+    'is_custom' => false,
+    'from_studio' => true,
+  ),
+  'irw_myreviews_activities_tasks' => 
+  array (
+    'id' => '96b6a9dc-c4a3-802e-d4b9-5e4465c0e6b1',
+    'relationship_name' => 'irw_myreviews_activities_tasks',
+    'lhs_module' => 'Irw_myreviews',
+    'lhs_table' => 'irw_myreviews',
+    'lhs_key' => 'id',
+    'rhs_module' => 'Tasks',
+    'rhs_table' => 'tasks',
+    'rhs_key' => 'parent_id',
+    'join_table' => NULL,
+    'join_key_lhs' => NULL,
+    'join_key_rhs' => NULL,
+    'relationship_type' => 'one-to-many',
+    'relationship_role_column' => 'parent_type',
+    'relationship_role_column_value' => 'Irw_myreviews',
+    'reverse' => '0',
+    'deleted' => '0',
+    'readonly' => true,
+    'rhs_subpanel' => NULL,
+    'lhs_subpanel' => NULL,
+    'is_custom' => true,
+    'relationship_only' => false,
+    'for_activities' => false,
+    'from_studio' => true,
+  ),
+  'irw_myreviews_assigned_user' => 
+  array (
+    'id' => '9c6e3ab1-b5fb-a46e-b66b-5e4465fbd89e',
+    'relationship_name' => 'irw_myreviews_assigned_user',
+    'lhs_module' => 'Users',
+    'lhs_table' => 'users',
+    'lhs_key' => 'id',
+    'rhs_module' => 'Irw_myreviews',
+    'rhs_table' => 'irw_myreviews',
+    'rhs_key' => 'assigned_user_id',
+    'join_table' => NULL,
+    'join_key_lhs' => NULL,
+    'join_key_rhs' => NULL,
+    'relationship_type' => 'one-to-many',
+    'relationship_role_column' => NULL,
+    'relationship_role_column_value' => NULL,
+    'reverse' => '0',
+    'deleted' => '0',
+    'readonly' => true,
+    'rhs_subpanel' => NULL,
+    'lhs_subpanel' => NULL,
+    'relationship_only' => false,
+    'for_activities' => false,
+    'is_custom' => false,
+    'from_studio' => true,
+  ),
+  'irw_myreviews_activities_calls' => 
+  array (
+    'id' => 'a2ca5449-e2eb-6b38-1795-5e446505d1dd',
+    'relationship_name' => 'irw_myreviews_activities_calls',
+    'lhs_module' => 'Irw_myreviews',
+    'lhs_table' => 'irw_myreviews',
+    'lhs_key' => 'id',
+    'rhs_module' => 'Calls',
+    'rhs_table' => 'calls',
+    'rhs_key' => 'parent_id',
+    'join_table' => NULL,
+    'join_key_lhs' => NULL,
+    'join_key_rhs' => NULL,
+    'relationship_type' => 'one-to-many',
+    'relationship_role_column' => 'parent_type',
+    'relationship_role_column_value' => 'Irw_myreviews',
+    'reverse' => '0',
+    'deleted' => '0',
+    'readonly' => true,
+    'rhs_subpanel' => NULL,
+    'lhs_subpanel' => NULL,
+    'is_custom' => true,
+    'relationship_only' => false,
+    'for_activities' => false,
+    'from_studio' => true,
+  ),
+  'securitygroups_irw_myreviews' => 
+  array (
+    'id' => 'a891f165-fafc-361e-6bbd-5e44653e723b',
+    'relationship_name' => 'securitygroups_irw_myreviews',
+    'lhs_module' => 'SecurityGroups',
+    'lhs_table' => 'securitygroups',
+    'lhs_key' => 'id',
+    'rhs_module' => 'Irw_myreviews',
+    'rhs_table' => 'irw_myreviews',
+    'rhs_key' => 'id',
+    'join_table' => 'securitygroups_records',
+    'join_key_lhs' => 'securitygroup_id',
+    'join_key_rhs' => 'record_id',
+    'relationship_type' => 'many-to-many',
+    'relationship_role_column' => 'module',
+    'relationship_role_column_value' => 'Irw_myreviews',
+    'reverse' => '0',
+    'deleted' => '0',
+    'readonly' => true,
+    'rhs_subpanel' => NULL,
+    'lhs_subpanel' => NULL,
+    'relationship_only' => false,
+    'for_activities' => false,
+    'is_custom' => false,
+    'from_studio' => true,
+  ),
+  'irw_myreviews_activities_meetings' => 
+  array (
+    'id' => 'aeeead4b-9b9d-9e4f-c23f-5e4465662e4a',
+    'relationship_name' => 'irw_myreviews_activities_meetings',
+    'lhs_module' => 'Irw_myreviews',
+    'lhs_table' => 'irw_myreviews',
+    'lhs_key' => 'id',
+    'rhs_module' => 'Meetings',
+    'rhs_table' => 'meetings',
+    'rhs_key' => 'parent_id',
+    'join_table' => NULL,
+    'join_key_lhs' => NULL,
+    'join_key_rhs' => NULL,
+    'relationship_type' => 'one-to-many',
+    'relationship_role_column' => 'parent_type',
+    'relationship_role_column_value' => 'Irw_myreviews',
+    'reverse' => '0',
+    'deleted' => '0',
+    'readonly' => true,
+    'rhs_subpanel' => NULL,
+    'lhs_subpanel' => NULL,
+    'is_custom' => true,
+    'relationship_only' => false,
+    'for_activities' => false,
+    'from_studio' => true,
+  ),
+  'irw_myreviews_activities_emails' => 
+  array (
+    'id' => 'bb1f56cc-3015-616f-3255-5e4465302100',
+    'relationship_name' => 'irw_myreviews_activities_emails',
+    'lhs_module' => 'Irw_myreviews',
+    'lhs_table' => 'irw_myreviews',
+    'lhs_key' => 'id',
+    'rhs_module' => 'Emails',
+    'rhs_table' => 'emails',
+    'rhs_key' => 'parent_id',
+    'join_table' => NULL,
+    'join_key_lhs' => NULL,
+    'join_key_rhs' => NULL,
+    'relationship_type' => 'one-to-many',
+    'relationship_role_column' => 'parent_type',
+    'relationship_role_column_value' => 'Irw_myreviews',
+    'reverse' => '0',
+    'deleted' => '0',
+    'readonly' => true,
+    'rhs_subpanel' => NULL,
+    'lhs_subpanel' => NULL,
+    'is_custom' => true,
+    'relationship_only' => false,
+    'for_activities' => false,
+    'from_studio' => true,
+  ),
+  'irw_myreviews_irw_indicators_1' => 
+  array (
+    'rhs_label' => 'Indicadores',
+    'lhs_label' => 'ACPM',
+    'lhs_subpanel' => 'default',
+    'rhs_subpanel' => 'default',
+    'lhs_module' => 'Irw_myreviews',
+    'rhs_module' => 'Irw_indicators',
+    'relationship_type' => 'many-to-many',
+    'readonly' => true,
+    'deleted' => false,
+    'relationship_only' => false,
+    'for_activities' => false,
+    'is_custom' => false,
+    'from_studio' => true,
+    'relationship_name' => 'irw_myreviews_irw_indicators_1',
+  ),
+);
