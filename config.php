ae9729eed5491570998fa24594a77c873f38dcd6
martinezosoriod <martinezosoriod@gmail.com>
2020-02-17 11:28:26 -0500

    config index

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+  $space_config = array(
+    'db' => array(
+      'config' => 'localhost',
+      'user' => 'ns',
+      'password' => 'inocent',
+      'name' => 'ns_dv',
+      'type' => 'mysql',
+    ),
+    'languages' =>
+    array (
+      'en_us' => 'English (US)',
+      'es_es' => 'Español (ES)',
+    ),
+  );
+ ?>
