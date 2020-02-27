2caa79516e2e0f6be0f4f1c63bce2e45e487f06c
invreate <invreate@invreate.invreate.com>
2020-02-26 20:19:45 -0500

    bug db config:_overide

--- system/config_override.php
+++ system/config_override.php
@@ -2,5 +2,5 @@
 /***CONFIGURATOR***/
 $sugar_config['disable_persistent_connections'] = false;
 $sugar_config['default_language'] = 'es_ES';
-$sugar_config['dbconfig']['db_name'] = 'ns_dv';
+$sugar_config['dbconfig']['db_name'] = 'ns_kernel_general';
 /***CONFIGURATOR***/
